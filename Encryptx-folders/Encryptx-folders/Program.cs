fe141337f42805c1e4f83425e6325cf89a7e7be9
Anuj8611 <2k24.cs1e.2410975@gmail.com>
2024-12-05 22:23:12 +0530

    Add files via upload
    added code for folder packing

---
+++ Encryptx-folders/Encryptx-folders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Encryptx_folders
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Enter Path of folders");
+            string folders = Console.ReadLine();
+            if(Directory.Exists(folders))
+            {
+                Console.WriteLine("Folder exists");
+            }
+            Console.ReadLine();
+        }
+    }
+}
