8025421fe2ab34db5a391bef346402be19e62cc3
Anuj8611 <2k24.cs1e.2410975@gmail.com>
2024-12-07 18:26:16 +0530

    fix
    file size display

--- Encryptx-folders/Encryptx-folders/Program.cs
+++ Encryptx-folders/Encryptx-folders/Program.cs
@@ -1,52 +1,47 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-
 
 namespace Encryptx_folders
 {
     class Program
     {
         static void Main(string[] args)
-
         {
             Console.WriteLine("Enter Path of folder: ");
-
             string folders = Console.ReadLine();
             Console.WriteLine();
+
             if (Directory.Exists(folders))
             {
-
                 Console.WriteLine("Enter Output path: ");
                 string output = Console.ReadLine();
                 Console.WriteLine();
-                
 
                 string[] files = Directory.GetFiles(folders); // Get all files in the folder
                 Console.WriteLine("**FILES PRESENT IN THE FOLDER**");
                 Console.WriteLine();
 
+                // Print headers
+                Console.WriteLine("{0,-40} {1,10} {2,6}", "File Name", "Size", "Unit");
+                Console.WriteLine(new string('-', 60));
+
                 foreach (string file in files)
                 {
                     FileInfo finfo = new FileInfo(file);
-                    Console.Write(Path.GetFileName(file)); // Display each file
-                    Console.Write("       " + finfo.Length);
-                    Console.Write("       " + "BYTES");
-                    Console.WriteLine("\n");
-                    
+                    string fileName = Path.GetFileName(file);
+                    long fileSize = finfo.Length;
+
+                    // Format output for alignment
+                    Console.WriteLine("{0,-40} {1,10} {2,6}", fileName, fileSize, "BYTES");
                 }
 
                 Console.WriteLine();
                 Console.WriteLine("Adding files to archive......");
                 Console.WriteLine();
-
             }
             else
             {
-                Console.WriteLine("Folder does not exists");
+                Console.WriteLine("Folder does not exist");
                 Console.ReadKey();
             }
 
