5785c93850748b766794f4234d276f84c9d3609f
Anuj8611 <2k24.cs1e.2410975@gmail.com>
2024-12-07 18:20:10 +0530

    update files show code with size

--- Encryptx-folders/Encryptx-folders/Program.cs
+++ Encryptx-folders/Encryptx-folders/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,26 +13,41 @@ namespace Encryptx_folders
         static void Main(string[] args)
 
         {
-            Console.WriteLine("Enter Path of folder");
+            Console.WriteLine("Enter Path of folder: ");
+
             string folders = Console.ReadLine();
-            if(Directory.Exists(folders))
+            Console.WriteLine();
+            if (Directory.Exists(folders))
             {
-                
-                Console.WriteLine("Enter Output path ");
+
+                Console.WriteLine("Enter Output path: ");
                 string output = Console.ReadLine();
+                Console.WriteLine();
+                
 
                 string[] files = Directory.GetFiles(folders); // Get all files in the folder
+                Console.WriteLine("**FILES PRESENT IN THE FOLDER**");
+                Console.WriteLine();
 
-                 foreach (string file in files)
-                 {
-                 Console.WriteLine(file); // Display each file
-                 }
-                
+                foreach (string file in files)
+                {
+                    FileInfo finfo = new FileInfo(file);
+                    Console.Write(Path.GetFileName(file)); // Display each file
+                    Console.Write("       " + finfo.Length);
+                    Console.Write("       " + "BYTES");
+                    Console.WriteLine("\n");
+                    
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Adding files to archive......");
+                Console.WriteLine();
+
             }
             else
             {
                 Console.WriteLine("Folder does not exists");
-                Console.Readkey();
+                Console.ReadKey();
             }
 
             Console.ReadLine();
